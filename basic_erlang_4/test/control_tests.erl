750135d2d91dd2cd6a92aa91924780e72eeea9f6
Robin Larsson <robin.larsson@ericsson.com>
2017-05-22 16:45:32 +0200

    Adds more control tests and updates the presentation with control

--- basic_erlang_4/test/control_tests.erl
+++ basic_erlang_4/test/control_tests.erl
@@ -16,5 +16,14 @@ many_slow_actions_test_() ->
        fun() -> timer:sleep(400), ok end]
      }
     }.
-	      
-	      
+
+even_more_slow_actions_test_() ->
+    {inparallel,
+     [{timeout, 0.25, fun() -> timer:sleep(200), ok end},
+      {timeout, 0.4, [fun() -> timer:sleep(300), ok end,
+		      fun() -> timer:sleep(300), ok end
+		     ]},
+      {timeout, 1, {"Long timeout!",
+		    fun() -> timer:sleep(900) end}}
+     ]
+    }.
