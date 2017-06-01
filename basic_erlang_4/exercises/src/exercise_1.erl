16bd0dcd8beb772e9ff52d850641da4312e7b88c
Robin Larsson <robin.larsson@ericsson.com>
2017-06-01 17:03:07 +0200

    Updates the exercises

--- basic_erlang_4/exercises/src/exercise_1.erl
+++ basic_erlang_4/exercises/src/exercise_1.erl
@@ -2,6 +2,12 @@
 -export([get_hostnames_of_all_connected_nodes/0]).
 
 get_hostnames_of_all_connected_nodes() ->
+    %% A node has the form user@host,
+    %% we want to only get the host part.
+    Nodes = get_nodes(),
+    [remove_user_part(Node) || Node <- Nodes].
+
+remove_user_part(Node) ->
     throw(not_implemented_yet).
 
 get_nodes() ->
